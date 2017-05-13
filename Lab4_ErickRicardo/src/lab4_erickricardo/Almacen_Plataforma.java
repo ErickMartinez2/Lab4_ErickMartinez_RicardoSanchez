8fa443b59a32c598ed57d96b733f24980b941fc5
Nichole Sanchez <Nichole Sanchez>
2017-05-12 18:25:14 -0600

    Otra cifra creada

--- Lab4_ErickRicardo/src/lab4_erickricardo/Almacen_Plataforma.java
+++ Lab4_ErickRicardo/src/lab4_erickricardo/Almacen_Plataforma.java
@@ -1,5 +1,6 @@
 package lab4_erickricardo;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Almacen_Plataforma extends Almacen{
@@ -16,7 +17,75 @@ public class Almacen_Plataforma extends Almacen{
 
     @Override
     public boolean Cifrado_Transposicion() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String Contra = ES.getContrasena();
+        String Clave = ES.getClave();
+        Random rd = new Random();
+        Object[][] Cifrado = new Object[4][Contra.length()];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < Contra.length(); j++) {
+                char ch;
+                int Dis;
+                String AlfabetoM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                String Alfabetom = "abcdefghijklmnopqrstuvwxyz";
+                if (i == 0) {
+                    Cifrado[i][j] = Contra.charAt(j);
+                } else if (i == 1) {
+                    if (((int) (Contra.charAt(j))) > 91) {
+                        ch = Contra.charAt(j);
+                        Dis = Alfabetom.indexOf(ch);
+                        Cifrado[i][j] = Dis;
+                    } else {
+                        ch = Contra.charAt(j);
+                        Dis = AlfabetoM.indexOf(ch);
+                        Cifrado[i][j] = Dis;
+                    }
+
+                } else if (i == 2) {
+                    Cifrado[i][j] = Clave.charAt(j);
+                } else if (i == 3) {
+                    if (((int) (Contra.charAt(j))) > 91) {
+                        ch = Contra.charAt(j);
+                        Dis = Alfabetom.indexOf(ch);
+                        char ch1 = Clave.charAt(j);
+                        int Dis1 = ((int)Clave.charAt(j));
+                        for (int k = 0; k < Dis; k++) {
+                            Dis1++;
+                            if(Dis1 == 123){
+                                Dis1 = 97;
+                            }
+                        }
+                        Cifrado[i][j] = ((char)Dis1);
+                    }else{
+                       ch = Contra.charAt(j);
+                        Dis = AlfabetoM.indexOf(ch);
+                        char ch1 = Clave.charAt(j);
+                        int Dis1 = ((int)Clave.charAt(j));
+                        for (int k = 0; k < Dis; k++) {
+                            Dis1++;
+                            if(Dis1 == 91){
+                                Dis1 = 65;
+                            }
+                        }
+                        Cifrado[i][j] = ((char)Dis1);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < Contra.length(); j++) {
+                System.out.print("[" + Cifrado[i][j] + "] ");
+            }
+            System.out.println("");
+        }
+        System.out.println("Ingrese la contrasena");
+        String Comp = sc.next();
+        System.out.println("Ingrese la clave");
+        String Comp1 = sc.next();
+        if(Comp.equals(Contra) && Comp1.equals(Clave)){
+            return true;
+        }else{
+            return false;
+        }  
     }
 
     @Override
@@ -56,7 +125,18 @@ public class Almacen_Plataforma extends Almacen{
 
     @Override
     public boolean Cifrado_Binario() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String clave = ES.getClave();
+        String Binario = "";
+        String BinarioReversa = "";
+        for (int i = 0; i < clave.length(); i++) {
+            Binario += Integer.toBinaryString(clave.charAt(i));
+        }
+        System.out.println("Binario Original " + Binario);
+        for (int i = Binario.length() - 1; i > 0; i--) {
+            BinarioReversa += Binario.charAt(i);
+        }
+        System.out.println("Binario al reves " + BinarioReversa);
+        return true;
     }
 
     @Override
